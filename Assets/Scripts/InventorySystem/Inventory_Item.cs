191a1dae3a67139e13352e11ce8f8de8f5166cc6
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-01-06 02:06:03 +0700

    Craft UI frontend is working

--- Assets/Scripts/InventorySystem/Inventory_Item.cs
+++ Assets/Scripts/InventorySystem/Inventory_Item.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using UnityEngine;
 
 [Serializable]
@@ -54,4 +55,84 @@ public class Inventory_Item
     public bool CanAddStack() => stackSize < itemData.maxStackSize;
     public void AddStack() => stackSize++;
     public void RemoveStack() => stackSize--;
+
+
+    public string GetItemInfo()
+    {
+        if (itemData.itemType == ItemType.Material)
+        {
+            return "Used for crafting.";
+        }
+
+        if (itemData.itemType == ItemType.Consumable)
+        {
+            return itemData.itemEffect.effectDescription;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("");
+
+        foreach (var mod in modifiers)
+        {
+            string modType = GetStatNameByType(mod.statType);
+            string modValue = IsPercentageStat(mod.statType) ? mod.value.ToString() + "%" : mod.value.ToString();
+            sb.AppendLine("+ " + modValue + " " + modType);
+        }
+
+        if (itemEffect != null)
+        {
+            sb.AppendLine("");
+            sb.AppendLine("Unique Effect:");
+            sb.AppendLine(itemEffect.effectDescription);
+        }
+
+        return sb.ToString();
+    }
+
+    private string GetStatNameByType(StatType type)
+    {
+        switch (type)
+        {
+            case StatType.MaxHealth: return "Max Health";
+            case StatType.HealthRegen: return "Health Regenaration";
+            case StatType.Strength: return "Strength";
+            case StatType.Agility: return "Agility";
+            case StatType.Vitality: return "Vitality";
+            case StatType.Intelligence: return "Intelligence";
+            case StatType.AttackSpeed: return "Attack Speed";
+            case StatType.Damage: return "Damage";
+            case StatType.CritChance: return "Crit Chance";
+            case StatType.CritPower: return "Crit Power";
+            case StatType.ArmorReduction: return "Armor Reduction";
+            case StatType.FireDamage: return "Fire Damage";
+            case StatType.IceDamage: return "Ice Damage";
+            case StatType.LightningDamage: return "Lightning Damage";
+            case StatType.Armor: return "Armor";
+            case StatType.Evasion: return "Evasion";
+            case StatType.IceResistance: return "Ice Resistance";
+            case StatType.FireResistance: return "Fire Resistance";
+            case StatType.LightningResistance: return "Lightning Resistance";
+            default: return "Unknown Stat";
+        }
+    }
+
+    private bool IsPercentageStat(StatType type)
+    {
+        switch (type)
+        {
+            case StatType.CritChance:
+            case StatType.CritPower:
+            case StatType.ArmorReduction:
+            case StatType.Evasion:
+            case StatType.IceResistance:
+            case StatType.FireResistance:
+            case StatType.LightningResistance:
+            case StatType.AttackSpeed:
+                return true;
+            default:
+                return false;
+
+        }
+    }
 }
